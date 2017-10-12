19ed2ec894c650b9aff7a5f1cce8ff5248fb44f5
Austin Derbique <A01967241@aggies.usu.edu>
2017-10-12 12:42:52 -0600

    working

--- assn5/analyze_files.py
+++ assn5/analyze_files.py
@@ -1,4 +1,4 @@
-import os, sys
+import os, sys, pickle
 from SummaryStatistics import SS
 
 def create_lists(filename):
@@ -17,22 +17,26 @@ def create_lists(filename):
     for line in file:
         if count > -1:
             parts = line.split(',')
-            time = parts[0]
+            time = int(parts[0])
             #users[count] = parts[1]
             #sessions[count] = parts[2]
             #events[count] = parts[3].rstrip('\n')
 
-            users.append(parts[1])
-            sessions.append(parts[2])
-            events.append(parts[3].rstrip('\n'))
+            users.append(int(parts[1]))
+            sessions.append(int(parts[2]))
+            events.append(int(parts[3].rstrip('\n')))
             #time,users[count],sessions[count],events[count] = line.split(',')
         count = count + 1
     file.close()
     return users, sessions, events
+
+def write_summary(fn,summary):
+    with open(fn,'wb') as fp:
+        pickle.dump(summary,fp)
+            
     
 day_user,day_session,day_event = create_lists("OSSUsersSessionsEventsByDay20160101-20170930.csv")
-hour_user,hour_session_hour_event = create_lists("OSSUsersSessionsEventsByHour20160101-20170930.csv")
-
+hour_user,hour_session, hour_event = create_lists("OSSUsersSessionsEventsByHour20160101-20170930.csv")
 
 day_user = SS(day_user)
 day_session = SS(day_session)
@@ -41,3 +45,5 @@ day_event = SS(day_event)
 hour_user = SS(hour_user)
 hour_session = SS(hour_session)
 hour_event = SS(hour_event)
+
+write_summary('day_user.csv',day_user)
